27b2411b84e310d1c0dce6e732e6c1ab9898a9df
alperduran217 <alperduran217@gmail.com>
2018-02-11 03:43:01 +0300

    External Template Added

---
+++ app/nav-menu/nav-menu.component.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.
+  module('navMenu').
+  component('navMenu', {
+    templateUrl: 'nav-menu/nav-menu.template.html',
+    controller: function OilListController() {
+
+    }
+  });
---
+++ app/nav-menu/nav-menu.component.spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('navMenu', function() {
+
+  // Load the module that contains the `phoneList` component before each test
+  beforeEach(module('navMenu'));
+
+  // Test the controller
+  describe('OilListController', function() {
+
+  });
+
+});
